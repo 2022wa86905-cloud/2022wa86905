0ec8a4cd022310a215b6e05c8db3724ca83ef9eb
2022wa86905-cloud <2022wa86905@wilp.bits-pilani.ac.in>
2026-09-24 17:44:31 +0530

    Update App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -2,7 +2,7 @@ package com.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("DevOps CI/CD Pipeline Initialized.");
+        System.out.println("DevOps CI/CD Pipeline Initialized. Task 4 Complete!");
     }
 
     public String getStatus() {
